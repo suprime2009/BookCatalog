cc8c4e86358f785cca2ebcde2f6e0257097e4537
suprime2009 <apolopavel@gmail.com>
2016-01-10 19:14:17 +0200

    sanday

--- BookLibrary-cdi/src/main/java/com/softserveinc/action/managebook/CreateBookAction.java
+++ BookLibrary-cdi/src/main/java/com/softserveinc/action/managebook/CreateBookAction.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -25,21 +26,27 @@ import com.softserveinc.model.persist.facade.BookFacadeLocal;
 import com.softserveinc.model.session.manager.BookManagerLocal;
 
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class CreateBookAction {
 	
 	private String bookName;
 	private String publisher;
 	private Integer yearPublished;
 	private String isbn;
-	private Set<Author> authors;
-	private String firstName;
-	private String secondName;
-	private Author author;
+	
+
+	
+	
+	private boolean createDone;
+	
+	public boolean getCreateDone() {
+		return createDone;
+		
+	}
 	
 	private Book bookAfterCreate = null;
 	
-	private List<Author> authorAutocomplete;
+
 	
 	@EJB
 	private AuthorFacadeLocal authorFacade;
@@ -51,70 +58,14 @@ public class CreateBookAction {
 	private BookFacadeLocal bookfacade;
 	
 	public CreateBookAction() {
-		authors = new HashSet<Author>();
+	
 
 		System.out.println("BEAN CREATED");
 	}
 	
-	public List<String> autocompleteSecondName(String prefix) {
-		secondName = prefix;
-		authorAutocomplete = authorFacade.findAuthorsBySecondName(prefix);
-        List<String> result = new ArrayList<String>();
-        if ((prefix == null) || (prefix.length() == 0)) {
-            for (int i = 0; i < 10; i++) {
-                result.add(authorAutocomplete.get(i).getSecondName());
-            }
-        } else {
-            Iterator<Author> iterator = authorAutocomplete.iterator();
-            while (iterator.hasNext()) {
-                Author elem = ((Author) iterator.next());
-                if ((elem.getSecondName() != null && elem.getSecondName().toLowerCase().indexOf(prefix.toLowerCase()) == 0)
-                    || "".equals(prefix)) {
-                    result.add(elem.getSecondName());
-                }
-            }
-        }
- 
-        return result;
-    }
-	
-	public List<String> autocompleteFirstName(String prefix) { 
-	    List<String> result = new ArrayList<String>();
-        if ((prefix == null) || (prefix.length() == 0)) {
-            for (int i = 0; i < 10; i++) {
-                result.add(authorAutocomplete.get(i).getFirstName());
-            }
-        } else {
-            Iterator<Author> iterator = authorAutocomplete.iterator();
-            while (iterator.hasNext()) {
-                Author elem = ((Author) iterator.next());
-                if ((elem.getFirstName() != null && elem.getFirstName().toLowerCase().indexOf(prefix.toLowerCase()) == 0)
-                    || "".equals(prefix)) {
-                    result.add(elem.getFirstName());
-                }
-            }
-        }
- 
-        return result;
-	}
-	
-	public Book getBookAfterCreate() {
-		return bookAfterCreate;
-	}
-		
-	public void checkIfAuthorExist(ActionEvent event) throws AbortProcessingException  {
-		FacesContext context = FacesContext.getCurrentInstance();
-		
-		System.out.println(firstName);
-		System.out.println(secondName);
-        
-		 author = authorFacade.findAuthorByFullName(firstName, secondName);
-		if (author == null) {
-			context.addMessage(null, new FacesMessage("Author not finded"));
-		} else {
-			context.addMessage(null, new FacesMessage("Author  finded"));
-			addAuthor();
-		}
+	@PreDestroy
+	public void destroy() {
+		System.out.println("BEAN DESTROYED");
 	}
 	
 	public void validateISBN(FacesContext context, UIComponent comp,
@@ -130,36 +81,29 @@ public class CreateBookAction {
 	            context.addMessage(comp.getClientId(context), message);
 		 }
 	}
-	
-	public void addAuthor() {
-		System.out.println("ADDED FIELD");
 		
-		authors.add(author);
-		System.out.println(firstName);
-		System.out.println(secondName);
-	}
-	
 	public void submit() {
 		System.out.println("SUBMIT");
 		System.out.println(bookName);
 		System.out.println(publisher);
 		System.out.println(yearPublished);
 		System.out.println(isbn);
-		Book book = new Book(bookName, isbn, publisher, yearPublished, authors);
+		Book book = new Book(bookName, isbn, publisher, yearPublished, null);
 		bookAfterCreate = bookManager.createBook(book);
-		
+		if (bookAfterCreate != null) {
+			createDone = true;
+		}				
 	}
 
 	
 	public void reset() {
+		System.out.println("RESET");
 		bookName = null;
 		publisher = null;
 		yearPublished = null;
 		isbn = null;
-		authors = null;
-		firstName = null;
-		secondName = null;
-		author = null;
+		bookAfterCreate = null;
+		System.out.println("RESET");
 	}
 	
 	public String getBookName() {
@@ -190,29 +134,7 @@ public class CreateBookAction {
 		this.isbn = isbn;
 	}
 
-	public Set<Author> getAuthors() {
-		return authors;
-	}
 
-	public void setAuthors(Set<Author> authors) {
-		this.authors = authors;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getSecondName() {
-		return secondName;
-	}
-
-	public void setSecondName(String secondName) {
-		this.secondName = secondName;
-	}
 
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
---
+++ BookLibrary-cdi/src/main/java/com/softserveinc/action/managebook/EditBookAction.java
@@ -0,0 +1,160 @@
+package com.softserveinc.action.managebook;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+
+import com.softserveinc.model.persist.entity.Author;
+import com.softserveinc.model.persist.entity.Book;
+import com.softserveinc.model.persist.facade.AuthorFacadeLocal;
+import com.softserveinc.model.persist.facade.BookFacadeLocal;
+
+@ManagedBean
+@RequestScoped
+public class EditBookAction {
+	
+	private String idBook;
+	private Book book;
+	
+	private String firstName;
+	private Set<Author> authors;
+	private String secondName;
+	private Author author;
+	
+	private List<Author> authorAutocomplete;
+	
+	@EJB
+	private BookFacadeLocal bookFacade;
+	
+	@EJB
+	AuthorFacadeLocal authorFacade;
+	
+	public EditBookAction(){
+		authors = new HashSet<Author>();
+	}
+	
+	public List<String> autocompleteSecondName(String prefix) {
+		secondName = prefix;
+		authorAutocomplete = authorFacade.findAuthorsBySecondName(prefix);
+        List<String> result = new ArrayList<String>();
+        if ((prefix == null) || (prefix.length() == 0)) {
+            for (int i = 0; i < 10; i++) {
+                result.add(authorAutocomplete.get(i).getSecondName());
+            }
+        } else {
+            Iterator<Author> iterator = authorAutocomplete.iterator();
+            while (iterator.hasNext()) {
+                Author elem = ((Author) iterator.next());
+                if ((elem.getSecondName() != null && elem.getSecondName().toLowerCase().indexOf(prefix.toLowerCase()) == 0)
+                    || "".equals(prefix)) {
+                    result.add(elem.getSecondName());
+                }
+            }
+        }
+ 
+        return result;
+    }
+	
+	public void addAuthor() {
+		System.out.println("ADDED FIELD");
+		
+		authors.add(author);
+		System.out.println(firstName);
+		System.out.println(secondName);
+	}
+	
+	public List<String> autocompleteFirstName(String prefix) { 
+	    List<String> result = new ArrayList<String>();
+        if ((prefix == null) || (prefix.length() == 0)) {
+            for (int i = 0; i < 10; i++) {
+                result.add(authorAutocomplete.get(i).getFirstName());
+            }
+        } else {
+            Iterator<Author> iterator = authorAutocomplete.iterator();
+            while (iterator.hasNext()) {
+                Author elem = ((Author) iterator.next());
+                if ((elem.getFirstName() != null && elem.getFirstName().toLowerCase().indexOf(prefix.toLowerCase()) == 0)
+                    || "".equals(prefix)) {
+                    result.add(elem.getFirstName());
+                }
+            }
+        }
+ 
+        return result;
+	}
+	
+	public void checkIfAuthorExist(ActionEvent event) throws AbortProcessingException  {
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		System.out.println(firstName);
+		System.out.println(secondName);
+        
+		 author = authorFacade.findAuthorByFullName(firstName, secondName);
+		if (author == null) {
+			context.addMessage(null, new FacesMessage("Author not finded"));
+		} else {
+			context.addMessage(null, new FacesMessage("Author  finded"));
+			addAuthor();
+		}
+	}
+	
+	public void loadBook() {
+		book = bookFacade.findById(idBook);
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public String getIdBook() {
+		return idBook;
+	}
+
+	public void setIdBook(String idBook) {
+		this.idBook = idBook;
+	}
+	
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+	
+
+	
+	
+
+}
